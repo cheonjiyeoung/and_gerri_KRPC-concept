c263c1ee41fd81e748ac9d58fca596042417d294
cheonjiyeoung <138146221+cheonjiyeoung@users.noreply.github.com>
2025-11-02 17:51:54 +0900

    Update Spot_controller.py

--- gerri/robot/examples/Spot/Spot_controller.py
+++ gerri/robot/examples/Spot/Spot_controller.py
@@ -3,14 +3,18 @@ class SpotController:
     def __init__(self):
         pass
 
+    # method with default parameters example
     def move(self,vx=0.0,vy=0.0,vth=0.0):
         print(f"Moving with velocities vx: {vx}, vy: {vy}, vth: {vth}")
 
+    # method with non-default parameters example
     def move_waypoint(self,poi):
         print(f"Moving to waypoint: {poi}")
 
+    # method without parameters example
     def test_method(self):
         print("This is a test method in SpotController")
 
+    # private method example (for commander_builder test)
     def _some_private_method(self):
         print("This is a private method and should not be called directly.")
