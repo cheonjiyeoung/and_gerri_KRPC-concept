a080b00e34f468c294c75898eaead04347a2ba26
cheonjiyeoung <138146221+cheonjiyeoung@users.noreply.github.com>
2025-11-02 17:54:34 +0900

    Translate comments in Spot_robot.py to English
    Updated comments from Korean to English for clarity.

--- gerri/robot/examples/Spot/Spot_robot.py
+++ gerri/robot/examples/Spot/Spot_robot.py
@@ -6,9 +6,12 @@ from gerri.robot.utils.robot_sub_controller import RobotSubController
 from gerri.robot.examples.Spot.Spot_controller import SpotController
 
 
-# 테스트 코드
+# Test
+# 1. create robot_controller. (기존 sub_controller + base_controller -> 명칭 변경 예정)
 sub_controller = RobotSubController(robot_controller=SpotController())
 # sub_controller.connect()
+
+# 2. exac command test. 실제로는 command_handler호출이 아닌 webrtc를 통해 받은 커맨드 처리
 command1 = {"topic": "move", "value": {"vx": 2.0, "vy": 1.0, "vth": 0.5}}
 sub_controller.command_handler(command1)
 command2 = {"topic": "move_waypoint", "value": {"poi": [1.0, 2.0, 0.0]}}
